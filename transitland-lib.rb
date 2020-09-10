9178c28803df062e6c443773790d441efaced6d2
Ian Rees <ian@ianrees.net>
2020-09-10 14:36:48 -0700

    Update to signed (test)

--- transitland-lib.rb
+++ transitland-lib.rb
@@ -1,16 +1,16 @@
 class TransitlandLib < Formula
   desc "Library and tool for reading, writing, and processing transit data"
   homepage "https://github.com/interline-io/transitland-lib"
-  version "v0.6.0"
+  version "v0.6.1-rc5"
   bottle :unneeded
 
   if OS.mac?
     url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-macos"
-    sha256 "7ccb6092496831e7ab09c0b27aa06d64809b394844782e1bae6e6636f35d7a9b"
+    sha256 "36dc20c87ec0bc6a6703a246576ef9e29132fa3feb09d5f37180ac9150adc91d"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-linux"
-      sha256 "594b425c32873f0b607f2e3fa305d1a0debe301f3eec4a24182b705e2b77270b"
+      sha256 "6c971cb172d13e3b4b810d00046627378fbefd09728a3127207e87967353f686"
     end
   end
 
