7da62c6ea2bf8a4dd687bc1edfa6e5b45f37e091
Ian Rees <ian@ianrees.net>
2021-02-19 14:47:36 -0800

    Bump version

--- transitland-lib.rb
+++ transitland-lib.rb
@@ -1,16 +1,16 @@
 class TransitlandLib < Formula
   desc "Library and tool for reading, writing, and processing transit data"
   homepage "https://github.com/interline-io/transitland-lib"
-  version "v0.7.1"
+  version "v0.7.2"
   bottle :unneeded
 
   if OS.mac?
     url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-macos.zip"
-    sha256 "7de930fca5894554d987a2a9b21853fa4f1916107b1428c9d30dc4e93a87b81a"
+    sha256 "45e313abea742cd6bcd0925a91b77e9d9f81d8da2d0f9b5961337be9bc7c8b2a"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-linux"
-      sha256 "20f39331a634a219be019234e5882d02d45fff7b2d963cfa0a8b2a1e52e2a772"
+      sha256 "6672bdb344f0a66241f31db3f670b0c0d85e45585b85393224d433b9861b80ad"
     end
   end
 
