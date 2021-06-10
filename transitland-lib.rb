5252f424f23408c896f67703e6c80d87cebcd179
Drew Dara-Abrams <drew@interline.io>
2021-06-10 12:08:31 -0700

    transitland-lib v0.8.4
    https://github.com/interline-io/transitland-lib/releases/tag/v0.8.4

--- transitland-lib.rb
+++ transitland-lib.rb
@@ -1,16 +1,16 @@
 class TransitlandLib < Formula
   desc "Library and tool for reading, writing, and processing transit data"
   homepage "https://github.com/interline-io/transitland-lib"
-  version "v0.8.1"
+  version "v0.8.4"
   bottle :unneeded
 
   if OS.mac?
     url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-macos.zip"
-    sha256 "31dda70c7ce64b5343c83a24d363f75c306590790583d7629952dab6630cd5f8"
+    sha256 "b186a0f4119096346dd3de01d62fba5f1245ce1d8ae589ba055812ac3578a3ad"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-linux"
-      sha256 "78ef09047e39f50d95bce49dfbe72a7e7a7846f8a1610642fd3a6c08d67b4bae"
+      sha256 "ead040ab305317e2200f7d550b3ea006ae7bdc3ac981382df6d6e2c31c2ee22a"
     end
   end
 
