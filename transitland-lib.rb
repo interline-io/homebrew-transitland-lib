99741af420cc0c9fa3f2e81ba8d81d1b2dbb7245
Drew Dara-Abrams <drew@interline.io>
2021-10-26 10:03:05 -0700

    fixing a Homebrew warning
    > Calling bottle :unneeded is deprecated! There is no replacement.

--- transitland-lib.rb
+++ transitland-lib.rb
@@ -2,7 +2,6 @@ class TransitlandLib < Formula
   desc "Library and tool for reading, writing, and processing transit data"
   homepage "https://github.com/interline-io/transitland-lib"
   version "v0.8.6"
-  bottle :unneeded
 
   url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-macos.zip"
   sha256 "285103e01affa74e20423be5ab5dd9801d3910ed077e2e89ae4448668878ff52"
