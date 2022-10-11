90bc927e431efd63477175487c04e3fe0be15f9b
Drew Dara-Abrams <drew@interline.io>
2022-10-10 20:52:20 -0700

    transitland-lib v0.11.1
    https://github.com/interline-io/transitland-lib/releases/tag/v0.11.1

--- transitland-lib.rb
+++ transitland-lib.rb
@@ -1,10 +1,10 @@
 class TransitlandLib < Formula
   desc "Library and tool for reading, writing, and processing transit data"
   homepage "https://github.com/interline-io/transitland-lib"
-  version "v0.10.3"
+  version "v0.11.1"
 
   url "https://github.com/interline-io/transitland-lib/releases/download/#{version}/transitland-macos.zip"
-  sha256 "ef63686440703c15e93dcb9579bf8401456f80dbb8f9694d6c4255be6fd1a9d3"
+  sha256 "0b66f92800c5b885b26f6879c9ef3ac601827821530f4a9a14485c31f80cce03"
 
   def install
     bin.install 'transitland'
